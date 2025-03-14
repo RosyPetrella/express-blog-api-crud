3c281bce0e66fd346ab5e20ab8f0d6802f0910aa
RosyPetrella <petrellamr@gmail.com>
2025-03-14 15:32:23 +0100

    create post controller with basic CRUD functions

---
+++ controller/post_controller.js
@@ -0,0 +1,23 @@
+function index(req, res) {
+  res.json(posts);
+}
+
+function show(req, res) {
+  res.send(`Show post with id: ${req.params.id}`);
+}
+
+function store(req, res) {
+  res.send(`Create a new post`);
+}
+
+function update(req, res) {
+  res.send(`Update post with id: ${req.params.id}`);
+}
+
+function modify(req, res) {
+  res.send(`Modify post with id: ${req.params.id}`);
+}
+
+function destroy(req, res) {
+  res.send(`Delete post with id: ${req.params.id}`);
+}
--- server.js
+++ server.js
@@ -1,3 +1,15 @@
+/* Come prima cosa, creiamo un controller per i nostri post, 
+in una cartella controllers.
+All’interno, prepariamo tutte le funzioni necessarie e copiamo in ciascuna
+ la logica delle funzioni che attualmente si trovano nel router 
+ (al momento restituiscono solo dei messaggi).
+Poi torniamo sul file delle rotte. 
+Qui importiamo le funzioni dichiarate nel controller e 
+le associamo alle varie rotte, come visto in classe.
+Testiamo su postman se chiamando gli endpoint riceviamo effettivamente 
+le stesse risposte che avevamo prima.
+Se tutto funziona, passiamo alla prossima milestone*/
+
 const express = require("express");
 const app = express();
 const port = 3003;
