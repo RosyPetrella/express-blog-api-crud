5698df01e477d633e3b504c33905bfde12f1e7ea
RosyPetrella <petrellamr@gmail.com>
2025-03-14 16:04:29 +0100

    Add post CRUD functionality with JSON responses and update routes to use slugs

--- controller/post_controller.js
+++ controller/post_controller.js
@@ -1,9 +1,26 @@
+const posts = require("../data/post_array");
+
+/*Index dovrà restituire la lista dei post in formato JSON
+Show dovrà restituire un singolo post in formato JSON
+Destroy dovrà eliminare un singolo post dalla lista, 
+stampare nel terminale (console.log) la lista aggiornata,
+e rispondere con uno stato 204 e nessun contenuto.*/
+
 function index(req, res) {
   res.json(posts);
 }
 
 function show(req, res) {
-  res.send(`Show post with id: ${req.params.id}`);
+  const postSlug = req.params.slug;
+  const post = posts.find((post) => post.slug === postSlug);
+
+  if (!post) {
+    res.status(404);
+    return res.json({
+      error: "404 Not Found",
+    });
+  }
+  res.json(post);
 }
 
 function store(req, res) {
@@ -19,7 +36,19 @@ function modify(req, res) {
 }
 
 function destroy(req, res) {
-  res.send(`Delete post with id: ${req.params.id}`);
+  const postSlug = req.params.slug;
+  const post = posts.find((post) => post.slug === postSlug);
+  console.log(post);
+
+  if (!post) {
+    res.status(404);
+    return res.json({
+      error: "404 Not Found",
+    });
+  }
+  posts.splice(posts.indexOf(post), 1);
+
+  res.json({ message: `Post with slug ${postSlug} deleted` });
 }
 
 module.exports = {
--- routers/post_list.js
+++ routers/post_list.js
@@ -1,24 +1,24 @@
 const express = require("express");
 const router = express.Router();
-const posts = require("..//immagini_e_posts/posts");
+// const posts = require("..//immagini_e_posts/posts");
 const postController = require("../controller/post_controller");
 
 //index
 router.get("/", postController.index);
 
 //show
-router.get("/:id", postController.show);
+router.get("/:slug", postController.show);
 
 //store
 router.post("/", postController.store);
 
 //update
-router.put("/:id", postController.update);
+router.put("/:slug", postController.update);
 
 //modify
-router.patch("/:id", postController.modify);
+router.patch("/:slug", postController.modify);
 
 //delete
-router.delete("/:id", postController.destroy);
+router.delete("/:slug", postController.destroy);
 
 module.exports = router;
